88c55b79093fc6666d64d68c7e504b2e79b6b7fe
Roman Tereshkov <tereshkov291993@gmail.com>
2022-04-06 05:20:00 +0300

    Хедер

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '',
+          DEFAULT: '#00FEFF',
+          dark: '#001E42',
+          },
+        },
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+}
